5078562159c112d5c3ca7fec3e445a6e30900210
kaiweicai <kaiweicai@163.com>
2024-06-07 01:20:33 +0800

    add white list

--- contracts/BTCF.sol
+++ contracts/BTCF.sol
@@ -8,9 +8,11 @@ import "@openzeppelin/contracts/utils/math/Math.sol";
 contract BTCF is ERC20, Ownable {
     using Math for uint256;
 
-    uint256 public constant FEE_PERCENT = 10; // 10% fee
+    uint256 public constant FEE_PERCENT = 1; // 1% fee
 
     address public feeReceiver;
+
+    mapping(address => bool) whiteList;
 
     event TransferWithFee(address indexed from, address indexed to, uint256 value, uint256 fee);
     event FeeWithdrawn(address indexed owner, uint256 amount);
@@ -18,36 +20,56 @@ contract BTCF is ERC20, Ownable {
     constructor(
         string memory name,
         string memory symbol,
-        uint256 initialSupply
+        uint256 initialSupply,
+        address minter
     ) ERC20(name, symbol) Ownable(_msgSender()) {
-        _mint(msg.sender, initialSupply);
+        _mint(minter, initialSupply);
         feeReceiver = msg.sender;
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
     }
 
     function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
         uint256 fee = (amount * FEE_PERCENT) / (100);
-        uint256 transferAmount = amount - (fee);
+        if (whiteList[_msgSender()]) {
+            _transfer(_msgSender(), recipient, amount);
+        } else {
+            uint256 transferAmount = amount - (fee);
 
-        _transfer(_msgSender(), recipient, transferAmount);
-        _transfer(_msgSender(), feeReceiver, fee);
+            _transfer(_msgSender(), recipient, transferAmount);
+            _transfer(_msgSender(), feeReceiver, fee);
 
-        emit TransferWithFee(_msgSender(), recipient, transferAmount, fee);
+            emit TransferWithFee(_msgSender(), recipient, transferAmount, fee);
+        }
+
         return true;
     }
 
     function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
-        uint256 fee = (amount * FEE_PERCENT) / 100;
-        uint256 transferAmount = amount - fee;
+        if (whiteList[_msgSender()]) {
+            _transfer(sender, recipient, amount);
+            _approve(sender, _msgSender(), allowance(sender, _msgSender()) - amount);
+        } else {
+            uint256 fee = (amount * FEE_PERCENT) / 100;
+            uint256 transferAmount = amount - fee;
+            _transfer(sender, recipient, transferAmount);
+            _transfer(sender, feeReceiver, fee);
+            _approve(sender, _msgSender(), allowance(sender, _msgSender()) - amount);
 
-        _transfer(sender, recipient, transferAmount);
-        _transfer(sender, feeReceiver, fee);
-        _approve(sender, _msgSender(), allowance(sender, _msgSender()) - amount);
+            emit TransferWithFee(sender, recipient, transferAmount, fee);
+        }
 
-        emit TransferWithFee(sender, recipient, transferAmount, fee);
         return true;
     }
 
     function setFeeReceiver(address _feeReceiver) public onlyOwner {
         feeReceiver = _feeReceiver;
+    }
+
+    function setWhiteList(address userAddress, bool isWitheList) public onlyOwner returns (bool) {
+        whiteList[userAddress] = isWitheList;
+        return true;
     }
 }
--- test/btcf/BTCFContractTest.spec.ts
+++ test/btcf/BTCFContractTest.spec.ts
@@ -1,42 +1,94 @@
 /* eslint-disable prefer-const */
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { loadFixture, time } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { BigNumber } from "bignumber.js";
+import { BN } from "bn.js";
 import { expect } from "chai";
 import exp from "constants";
 import { ethers } from "hardhat";
 
-const INITIAL_AMOUNT = 1000000;
-const PERCENT = 1000;
+const INITIAL_AMOUNT = new BigNumber("2100000000000000");
+const PERCENT = 100;
 const DENOMINATOR = 10000;
 
 describe("AirDrop", function () {
   async function deployTokenFixture() {
-    const [owner, account1, account2, account3] = await ethers.getSigners();
+    const [owner, account1, account2, account3, account4, account5] = await ethers.getSigners();
     const BTCF = await ethers.getContractFactory("BTCF");
-    const btcf = await BTCF.deploy("BTCF token", "BTCF", INITIAL_AMOUNT);
+    const btcf = await BTCF.deploy("BTCF token", "BTCF", INITIAL_AMOUNT.toNumber(), account3.address);
     await btcf.waitForDeployment();
 
     console.log("BTCF address: " + (await btcf.getAddress()));
 
-    return { btcf, owner, account1, account2, account3 };
+    return { btcf, owner, account1, account2, account3, account4, account5 };
   }
 
   describe("test", function () {
-    it("test init", async function () {
-      const { btcf, owner, account1, account2, account3 } = await loadFixture(deployTokenFixture);
+    it("test init and transfer", async function () {
+      const { btcf, owner, account1, account2, account3, account4 } = await loadFixture(deployTokenFixture);
+      expect(await btcf.decimals()).to.be.equal(6);
       let btcfAddress = await btcf.getAddress();
-      expect(await btcf.balanceOf(owner)).to.be.equal(INITIAL_AMOUNT);
+      expect(await btcf.owner()).to.be.equal(owner.address);
+      expect(await btcf.balanceOf(account3.address)).to.be.equal(INITIAL_AMOUNT);
       const transfer_amount = 10000;
-      await btcf.transfer(account1, transfer_amount);
+      await btcf.connect(account3).transfer(account1, transfer_amount, { from: account3.address });
       expect(await btcf.balanceOf(account1)).to.be.equal((transfer_amount * (DENOMINATOR - PERCENT)) / DENOMINATOR);
-      expect(await btcf.balanceOf(owner)).to.be.equal(
-        INITIAL_AMOUNT - (transfer_amount * (DENOMINATOR - PERCENT)) / 10000,
+      expect(await btcf.balanceOf(account3.address)).to.be.equal(
+        INITIAL_AMOUNT.minus((transfer_amount * DENOMINATOR) / DENOMINATOR),
       );
       await btcf.setFeeReceiver(account2);
       const transferAccount3Amount = 1000;
-      await btcf.transfer(account3, transferAccount3Amount);
+      await btcf.connect(account3).transfer(account4, transferAccount3Amount, { from: account3.address });
       const receivedFee = await btcf.balanceOf(account2);
       expect(receivedFee).to.be.equal((transferAccount3Amount * PERCENT) / DENOMINATOR);
+      await btcf.transferOwnership(account4.address);
+      expect(await btcf.owner()).to.be.equal(account4.address);
+    });
+
+    it("test init and transferFrom", async function () {
+      const { btcf, owner, account1, account2, account3, account4 } = await loadFixture(deployTokenFixture);
+      expect(await btcf.decimals()).to.be.equal(6);
+      expect(await btcf.owner()).to.be.equal(owner.address);
+      expect(await btcf.balanceOf(account3.address)).to.be.equal(INITIAL_AMOUNT);
+      const transfer_amount = 10000;
+      await btcf.connect(account3).approve(account1.address, transfer_amount, { from: account3.address });
+      await btcf.connect(account1).transferFrom(account3, account1, transfer_amount, { from: account1.address });
+      expect(await btcf.balanceOf(account1)).to.be.equal((transfer_amount * (DENOMINATOR - PERCENT)) / DENOMINATOR);
+      expect(await btcf.balanceOf(account3.address)).to.be.equal(
+        INITIAL_AMOUNT.minus((transfer_amount * DENOMINATOR) / DENOMINATOR),
+      );
+      await btcf.setFeeReceiver(account2);
+      const transferAccount3Amount = 1000;
+      await btcf.connect(account3).approve(account1.address, transfer_amount, { from: account3.address });
+      await btcf.connect(account1).transferFrom(account3, account4, transferAccount3Amount, { from: account1.address });
+      const receivedFee = await btcf.balanceOf(account2);
+      expect(receivedFee).to.be.equal((transferAccount3Amount * PERCENT) / DENOMINATOR);
+      await btcf.transferOwnership(account4.address);
+      expect(await btcf.owner()).to.be.equal(account4.address);
+    });
+
+    it("test init and whiteList", async function () {
+      const { btcf, owner, account1, account2, account3, account4, account5 } = await loadFixture(deployTokenFixture);
+      expect(await btcf.decimals()).to.be.equal(6);
+      expect(await btcf.owner()).to.be.equal(owner.address);
+      expect(await btcf.balanceOf(account3.address)).to.be.equal(INITIAL_AMOUNT);
+      const transfer_amount = 10000;
+      await btcf.connect(owner).setWhiteList(account3, true, { from: owner });
+      await btcf.connect(account3).transfer(account1, transfer_amount, { from: account3.address });
+      expect(await btcf.balanceOf(account1)).to.be.equal(transfer_amount);
+      expect(await btcf.balanceOf(account3.address)).to.be.equal(INITIAL_AMOUNT.minus(transfer_amount));
+
+      await btcf.setFeeReceiver(account2);
+      const transferAccount3Amount = 1000;
+      await btcf.connect(account3).transfer(account4, transferAccount3Amount, { from: account3.address });
+      const receivedFee = await btcf.balanceOf(account2);
+      expect(receivedFee).to.be.equal(0);
+
+      const transfer_from_amount = 2309;
+      await btcf.setWhiteList(account1, true);
+      await btcf.connect(account3).approve(account1, transfer_from_amount, { from: account3 });
+      await btcf.connect(account1).transferFrom(account3, account5, transfer_from_amount, { from: account1 });
+      expect(await btcf.balanceOf(account5)).to.be.equal(transfer_from_amount);
     });
   });
 });
